b140a24b81839cb9e9340d7bc0a1a89aaf9ba762
Asadupramod012 <pramodkumar.asadu@qodefarm.com>
2024-04-04 15:51:08 +0530

    updated Project Structure

--- nthras/config/settings.py
+++ nthras/config/settings.py
@@ -76,10 +76,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "db_name",
+        "USER": "root",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
