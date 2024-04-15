b76db9d7d6594147347b31ff93647533677adb68
Chetan0178 <chetan.chaudhari@qodefarm.com>
2024-04-15 17:27:45 +0530

    fixed conflict

--- apps/authentication/serializers.py
+++
@@ -1,8 +0,0 @@
-from djoser.serializers import UserCreateSerializer
-from .models import User
-
-
-class UserCreateSerializer(UserCreateSerializer):
-    class Meta(UserCreateSerializer.Meta):
-        model = User
-        fields = '__all__'
--- config/urls.py
+++ config/urls.py
@@ -1,21 +1,14 @@
-from django.contrib import admin
-<<<<<<< HEAD
-from django.urls import path, include
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/auth/', include('apps.authentication.url')),
-=======
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/company/', include('apps.company.urls')),
     path('api/v1/masters/', include('apps.masters.urls'))
->>>>>>> 3081a648342a84248f6bfdd1b3baa5cdce008710
+
 ]
 #below will handle media files uploaded when instance is created.
 
