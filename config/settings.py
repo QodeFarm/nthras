d4e08b4edc40999378f1ae62a770ce72b04c7679
Chetan0178 <chetan.chaudhari@qodefarm.com>
2024-04-25 11:11:21 +0530

    added env variables

--- config/settings.py
+++ config/settings.py
@@ -195,8 +195,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = EMAIL_USER
-EMAIL_HOST_PASSWORD= EMAIL_PASS 
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER') 
+EMAIL_HOST_PASSWORD= os.environ.get('EMAIL_PASS')  
 
 
 REST_FRAMEWORK = {    
