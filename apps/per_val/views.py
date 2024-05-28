4118c908dfcf777c1c7a84da2c5f52998f5139b0
Chetan0178 <chetan.chaudhari@qodefarm.com>
2024-05-28 11:07:20 +0530

    did changes in process-order-items API

--- apps/per_val/views.py
+++ apps/per_val/views.py
@@ -20,26 +20,35 @@ class TotalView(APIView):
                 return Response({'error': 'Invalid data format'}, status=status.HTTP_400_BAD_REQUEST)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
 class ProcessOrderView(APIView):
     def post(self, request):
-        serializer = OrderSerializer(data=request.data)
+        data = request.data
+        # Parse the 'last_cart_items' string to a JSON array
+        try:
+            data['last_cart_items'] = json.loads(data['last_cart_items'])
+        except json.JSONDecodeError:
+            return Response({"error": "Invalid JSON format in last_cart_items"}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = OrderSerializer(data=data)
         if serializer.is_valid():
             order_id = serializer.validated_data['order_id']
             cart_items = serializer.validated_data['last_cart_items']
-
+            # token = 'YOUR_BEARER_TOKEN'  
+            # headers = {
+            #     'Authorization': f'Bearer {token}',
+            #     'Content-Type': 'application/json'
+            # }        
             for item in cart_items:
                 payload = {
                     "product_id": item['ProductRetailerId'],
-                    "quantity": item['Quantity'],
+                    "Quantity": item['Quantity'],
                     "unit_price": item['Price'],
                     "order_id": order_id
                 }
                 try:
-                    response = requests.post('http://195.35.20.172:8000/api/v1/sales/order_items/', json=payload)
-                    response.raise_for_status()  # Raises an HTTPError if the response was an HTTP error
+                    response = requests.post("http://195.35.20.172:8000/api/v1/sales/order_items/", json=payload)
+                    response.raise_for_status() 
                 except requests.exceptions.RequestException as e:
                     return Response({"error": "Failed to send data", "details": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-            return Response({"status": "Success"}, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"status": "Success"}, status=status.HTTP_200_OK)
+        
