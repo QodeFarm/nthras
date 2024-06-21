65003f03edb7b188a859292603d9283fa9cfdeee
pavanpulicherla183 <pavankumar.pulicherla@datasteer.co.za>
2024-06-21 18:40:58 +0530

    updated and tested as per comments

--- apps/purchase/urls.py
+++ apps/purchase/urls.py
@@ -4,7 +4,7 @@ from rest_framework import routers, permissions
 from .views  import *
 
 router = routers.DefaultRouter()
-router.register(r'purchase_orders', PurchaseOrdersViewSet)
+router.register(r'purchase_orders_get', PurchaseOrdersViewSet)
 router.register(r'purchase_order_items', PurchaseorderItemsViewSet)
 router.register(r'purchase_invoice_orders', PurchaseInvoiceOrdersViewSet)
 router.register(r'purchase_invoice_items', PurchaseInvoiceItemViewSet)
@@ -16,6 +16,6 @@ router.register(r'purchase_price_list', PurchasePriceListViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('purchase-order/', purchaseOrdersCreateView.as_view(), name='purchase-order-list-create'),
-    path('purchase-order/<str:pk>/', purchaseOrdersCreateView.as_view(), name='purchase-order-detail-update-delete'),
+    path('purchase_order/', purchaseOrdersCreateView.as_view(), name='purchase-order-list-create'),
+    path('purchase_order/<str:pk>/', purchaseOrdersCreateView.as_view(), name='purchase-order-detail-update-delete'),
 ]
--- apps/purchase/views.py
+++ apps/purchase/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import *
@@ -5,7 +6,7 @@ from .serializers import *
 from config.utils_methods import *
 from config.utils_variables import *
 from rest_framework import generics
-from config.utils_methods import add_key_value_to_all_ordereddicts, create_multi_instance, delete_multi_instance, list_all_objects,create_instance,update_instance, update_multi_instance
+from config.utils_methods import add_key_value_to_all_ordereddicts, create_multi_instance, delete_multi_instance, list_all_objects, create_instance, update_instance, update_multi_instance, build_response
 from apps.sales.serializers import OrderAttachmentsSerializer,OrderShipmentsSerializer
 from apps.sales.models import OrderAttachments,OrderShipments
 from rest_framework import viewsets, generics, mixins as mi
@@ -103,6 +104,9 @@ class PurchasePriceListViewSet(viewsets.ModelViewSet):
     def update(self, request, *args, **kwargs):
         return update_instance(self, request, *args, **kwargs)
     
+
+##---------- ONE API - MULTIPLE API CALLS (CRUD OPERATIONS) ---------------
+
 class purchaseOrdersCreateView(generics.GenericAPIView, mi.ListModelMixin, mi.CreateModelMixin, mi.RetrieveModelMixin, mi.UpdateModelMixin, mi.DestroyModelMixin):
     queryset = PurchaseOrders.objects.all()
     serializer_class = PurchaseOrdersSerializer
@@ -122,85 +126,84 @@ class purchaseOrdersCreateView(generics.GenericAPIView, mi.ListModelMixin, mi.Cr
         purchase_order_items_data = given_data.pop('purchase_order_items')
         order_attachments_data = given_data.pop('order_attachments')
         order_shipments_data = given_data.pop('order_shipments')
- 
         # Create data in 'purchaseorder' model
         serializer = self.get_serializer(data=purchase_order_data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
-            data_1 = serializer.data
+            purchaseorder_data = serializer.data
  
             # Create data in 'purchaseorder_items' model
             purchase_order_id = serializer.data.get('purchase_order_id', None)  # from PurchaseOrder instance id is fetched
             add_key_value_to_all_ordereddicts(purchase_order_items_data, 'purchase_order_id', purchase_order_id) #in purchase_order_id replace purchase_order_id from new instance
-            data_2 = create_multi_instance(purchase_order_items_data, PurchaseorderItemsSerializer)
+            purchaseorder_items_data = create_multi_instance(purchase_order_items_data, PurchaseorderItemsSerializer)
  
-            # create data in 'order_attachments' model
-            add_key_value_to_all_ordereddicts(order_attachments_data, 'order_id', purchase_order_id) #in order_id replace purchase_order_id from new instance
-            data_3 = create_multi_instance(order_attachments_data, OrderAttachmentsSerializer)
+            # Fetching the 'order_type_id' by 'order_type'
+            order_type_val = order_attachments_data[0].get('order_type')
+            order_type = OrderTypes.objects.get(name=order_type_val)
+            order_type_id = order_type.order_type_id
+
+            # Updated user choice with associated ID in 'name_id_dictionary'
+            add_key_value_to_all_ordereddicts(order_attachments_data,'order_type_id',order_type_id)
+            #in order_id replace purchase_order_id from new instance
+            add_key_value_to_all_ordereddicts(order_attachments_data,'order_id',purchase_order_id) 
+            orderattachments_data = create_multi_instance(order_attachments_data,OrderAttachmentsSerializer)
  
             # create data in 'order_shipments' model
-            provided_val = order_shipments_data.get('order_type_id')
-            get_id = OrderTypes.objects.all()
-            name_id_dictionary = {}
-            for obj in get_id:
-                name = obj.name
-                id = obj.order_type_id
-                name_id_dictionary[name] = id
             order_shipments_data['order_id'] = purchase_order_id
-            order_shipments_data['order_type_id'] = name_id_dictionary.get(provided_val)
+            order_shipments_data['order_type_id'] = order_type_id
+
+            
+            
         
             serializer = OrderShipmentsSerializer(data=order_shipments_data)
             if serializer.is_valid(raise_exception=True):
                 serializer.save()
-                data_4 = serializer.data
+                ordershipments_data = serializer.data
  
-            if data_1 and data_2 and data_3 and data_4:  
-                custom_data = {
-                    "purchase_order": data_1,
-                    "purchase_order_items": data_2,
-                    "order_attachments": data_3,
-                    "order_shipments": data_4
-                }
-                return Response({
-                    'status': True,
-                    'message': 'Record created successfully',
-                    'data': custom_data
-                }, status=status.HTTP_201_CREATED)
+            if purchaseorder_data and purchaseorder_items_data and  orderattachments_data and ordershipments_data:  
+                custom_data = [
+                    {"purchase_order": purchaseorder_data},
+                    {"purchase_order_items": purchaseorder_items_data},
+                    { "order_attachments": orderattachments_data},
+                    {"order_shipments": ordershipments_data}
+                    ]
+                return build_response(1, "Record created successfully", custom_data, status.HTTP_201_CREATED)
             else:
-                return Response({
-                    'status': False,
-                    'message': 'Form validation failed',
-                    'errors': serializer.errors
-                }, status=status.HTTP_400_BAD_REQUEST)       
+                return build_response(0, "Record creation failed", [], status.HTTP_400_BAD_REQUEST)   
  
     def retrieve(self, request, *args, **kwargs):
-        pk = kwargs.get('pk')  # Access the pk from kwargs
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
- 
-        # Query PurchaseOrderItems model using the pk
-        items_related_data = PurchaseorderItems.objects.filter(purchase_order_id=pk)  # 'purchase_order_id' is the FK field
-        items_related_serializer = PurchaseorderItemsSerializer(items_related_data, many=True)
- 
-        # Get order_id value from PurchaseOrder instance
-        order_id = serializer.data.get('purchase_order_id')
- 
-        # Query OrderAttachments model using the order_id
-        attachments_related_data = OrderAttachments.objects.filter(order_id=str(pk))
-        attachments_related_serializer = OrderAttachmentsSerializer(attachments_related_data, many=True)
- 
-        # Query OrderShipments model using the order_id
-        shipments_related_data = OrderShipments.objects.filter(order_id=str(order_id))
-        shipments_related_serializer = OrderShipmentsSerializer(shipments_related_data, many=True)
- 
-        # Customizing the response data
-        custom_data = {
-            "purchase_order": serializer.data,
-            "purchase_order_items": items_related_serializer.data,
-            "order_attachments": attachments_related_serializer.data,
-            "order_shipments": shipments_related_serializer.data,
-        }
-        return Response(custom_data)
+        try:
+
+            pk = kwargs.get('pk')  # Access the pk from kwargs
+            instance = self.get_object()
+            serializer = self.get_serializer(instance)
+    
+            # Query PurchaseOrderItems model using the pk
+            items_related_data = PurchaseorderItems.objects.filter(purchase_order_id=pk)  # 'purchase_order_id' is the FK field
+            items_related_serializer = PurchaseorderItemsSerializer(items_related_data, many=True)
+    
+            # Get order_id value from PurchaseOrder instance
+            order_id = serializer.data.get('purchase_order_id')
+    
+            # Query OrderAttachments model using the order_id
+            attachments_related_data = OrderAttachments.objects.filter(order_id=str(pk))
+            attachments_related_serializer = OrderAttachmentsSerializer(attachments_related_data, many=True)
+    
+            # Query OrderShipments model using the order_id
+            shipments_related_data = OrderShipments.objects.filter(order_id=str(order_id))
+            shipments_related_serializer = OrderShipmentsSerializer(shipments_related_data, many=True)
+    
+            # Customizing the response data
+            custom_data = [
+                {"purchase_order": serializer.data},
+                {"purchase_order_items": items_related_serializer.data},
+                {"order_attachments": attachments_related_serializer.data},
+                {"order_shipments": shipments_related_serializer.data}
+            ]
+            return build_response(1, "Success", custom_data, status.HTTP_200_OK)
+
+        except Http404:
+            return build_response(0, "Record does not exist", [], status.HTTP_404_NOT_FOUND)
  
     def put(self, request, *args, **kwargs):
         return self.update(request, *args, **kwargs)
@@ -211,37 +214,37 @@ class purchaseOrdersCreateView(generics.GenericAPIView, mi.ListModelMixin, mi.Cr
         serializer = self.get_serializer(instance, data=request.data['purchase_order'], partial=partial)
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
-        data_1 = serializer.data
+        purchaseorder_data = serializer.data
         # Update purchase_order_items
         purchase_order_items_data = request.data.pop('purchase_order_items')
         pk = request.data['purchase_order'].get('purchase_order_id')
-        data_2 = update_multi_instance(purchase_order_items_data, pk, PurchaseOrders, PurchaseorderItems, PurchaseorderItemsSerializer)
+        purchaseorder_items_data = update_multi_instance(purchase_order_items_data, pk, PurchaseOrders, PurchaseorderItems, PurchaseorderItemsSerializer)
  
         # Update purchase_order_attachments
         order_attachments_data = request.data.pop('order_attachments')
         pk = request.data['purchase_order'].get('purchase_order_id')
-        data_3 = update_multi_instance(order_attachments_data, pk, PurchaseOrders, OrderAttachments, OrderAttachmentsSerializer, main_model_field_name='order_id')
+        orderattachments_data = update_multi_instance(order_attachments_data, pk, PurchaseOrders, OrderAttachments, OrderAttachmentsSerializer, main_model_field_name='order_id')
         # Update order_shipments
         order_shipments_data = request.data.pop('order_shipments')
         pk = request.data['purchase_order'].get('purchase_order_id')  # Fetch value from main model
-        data_4 = update_multi_instance(order_shipments_data, pk, PurchaseOrders, OrderShipments, OrderShipmentsSerializer, main_model_field_name='order_id')
+        ordershipments_data = update_multi_instance(order_shipments_data, pk, PurchaseOrders, OrderShipments, OrderShipmentsSerializer, main_model_field_name='order_id')
  
-        if data_1 and data_2 and data_3 and data_4:  
+        if purchaseorder_data and purchaseorder_items_data and orderattachments_data and ordershipments_data:  
             custom_data = {
-                "purchase_order": data_1,
-                "purchase_order_items": data_2,
-                "order_attachments": data_3,
-                "order_shipments": data_4
+                "purchase_order": purchaseorder_data,
+                "purchase_order_items": purchaseorder_items_data,
+                "order_attachments": orderattachments_data,
+                "order_shipments": ordershipments_data
             }
             return Response({
-                'status': True,
-                'message': 'Update successful',
+                'count': 1,
+                'message': 'Record updated successfully',
                 'data': custom_data
-            }, status=status.HTTP_200_OK)
+            },status=status.HTTP_200_OK)
         else:
             return Response({
-                'status': False,
-                'message': 'Form validation failed',
+                'count': 0,
+                'message': 'Record updation failed',
                 'errors': serializer.errors
             }, status=status.HTTP_400_BAD_REQUEST)
  
@@ -254,8 +257,10 @@ class purchaseOrdersCreateView(generics.GenericAPIView, mi.ListModelMixin, mi.Cr
             delete_multi_instance(pk, PurchaseOrders, OrderShipments, main_model_field_name='order_id')
             instance.delete()
  
-            return Response(status=status.HTTP_204_NO_CONTENT)
- 
+            # If Main model exists
+            return build_response(1, "Record deleted successfully", [], status.HTTP_204_NO_CONTENT)
+            
         except PurchaseOrders.DoesNotExist:
-            return Response({'error': 'Instance not found.'}, status=status.HTTP_404_NOT_FOUND)
+            # IF main model is not Found
+            return build_response(0, "Record deletion failed", [], status.HTTP_404_NOT_FOUND)
  
