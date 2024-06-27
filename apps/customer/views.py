f061f6c1e7655a1e23ae6bde0730733e0b33c4c0
Asadupramod012 <pramodkumar.asadu@qodefarm.com>
2024-06-27 22:30:02 +0530

    Updated Customers module

--- apps/customer/views.py
+++ apps/customer/views.py
@@ -15,6 +15,12 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.serializers import ValidationError
 
+# Set up basic configuration for logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Create a logger object
+logger = logging.getLogger(__name__) 
+
 # Create your views here.
 
 class LedgerAccountsViews(viewsets.ModelViewSet):
@@ -91,12 +97,7 @@ class CustomerAttachmentsViews(viewsets.ModelViewSet):
     def update(self, request, *args, **kwargs):
         return update_instance(self, request, *args, **kwargs)
     
-#==========================================================================   
-# Set up basic configuration for logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-# Create a logger object
-logger = logging.getLogger(__name__)  
+#==========================================================================  
 
 class CustomerCreateViews(APIView):
     
@@ -170,61 +171,65 @@ class CustomerCreateViews(APIView):
    
     @transaction.atomic
     def create(self, request, *args, **kwargs):
-
         given_data = request.data
- 
+
         # Extracting data from the request
         customer_data = given_data.pop('customer_data', None)
         customer_attachments_data = given_data.pop('customer_attachments', None)
         customer_addresses_data = given_data.pop('customer_addresses', None)
- 
+
         # Ensure mandatory customer data is present
-        if not customer_data or not customer_attachments_data or not customer_addresses_data:
-            logger.error("Customer data , Customer attachments_data and Customer Addresses are mandatory but not provided.")
-            return build_response(0, "Customer data , Customer attachments data and Customer Addresses data  are mandatory", [], status.HTTP_400_BAD_REQUEST)
- 
+        if not customer_data or not customer_addresses_data:
+            logger.error("Customer data and Customer Addresses are mandatory but not provided.")
+            return build_response(0, "Customer data and Customer Addresses data are mandatory", [], status.HTTP_400_BAD_REQUEST)
+
         response_data = {}
         errors = []
- 
+
         try:
             # Create customer
             customer_data = self.create_customer(customer_data)
             if not customer_data:
-                logger.error("customer creation failed.")
-                return build_response(0, "customer creation failed", [], status.HTTP_400_BAD_REQUEST)
- 
+                logger.error("Customer creation failed.")
+                return build_response(0, "Customer creation failed", [], status.HTTP_400_BAD_REQUEST)
+
             customer_id = customer_data.get('customer_id')
-            self.add_customer_id_to_attachments(customer_attachments_data, customer_id)
- 
-            # Create customer Attachment
-            attachments_data, attachments_errors = create_multi_instance(customer_attachments_data, CustomerAttachmentsSerializers)
-            if not attachments_data:
-                logger.error("customer Attachment creation failed.")
-                return build_response(0, "customer Attachment creation failed", [], status.HTTP_400_BAD_REQUEST, attachments_errors)
- 
+            if customer_attachments_data:
+                self.add_customer_id_to_attachments(customer_attachments_data, customer_id)
+
+                # Create customer Attachment
+                attachments_data, attachments_errors = create_multi_instance(customer_attachments_data, CustomerAttachmentsSerializers)
+                if not attachments_data:
+                    logger.error("Customer Attachment creation failed.")
+                    return build_response(0, "Customer Attachment creation failed", [], status.HTTP_400_BAD_REQUEST, attachments_errors)
+
+                errors.extend(attachments_errors)
+
             self.add_customer_id_to_addresses(customer_addresses_data, customer_id)
- 
+
             # Create customer Address
             addresses_data, addresses_errors = create_multi_instance(customer_addresses_data, CustomerAddressesSerializers)
             if not addresses_data:
-                logger.error("customer Address creation failed.")
-                return build_response(0, "customer Address creation failed", [], status.HTTP_400_BAD_REQUEST, addresses_errors)
- 
-            response_data = [
-                {"customer_data": customer_data},
-                {"customer_attachments": attachments_data},
-                {"customer_addresses":addresses_data}
-            ]
-            error = attachments_errors + addresses_errors
-            errors.extend(error)
- 
+                logger.error("Customer Address creation failed.")
+                return build_response(0, "Customer Address creation failed", [], status.HTTP_400_BAD_REQUEST, addresses_errors)
+
+            errors.extend(addresses_errors)
+
+            # Construct response_data in the desired order
+            response_data = {
+                "customer_data": customer_data,
+                "customer_addresses": addresses_data
+            }
+            if customer_attachments_data:
+                response_data["customer_attachments"] = attachments_data
+
             if errors:
                 logger.warning("Record created with some errors: %s", errors)
                 return build_response(1, "Record created with errors", response_data, status.HTTP_201_CREATED, errors)
- 
+
             logger.info("Record created successfully.")
             return build_response(1, "Record created successfully", response_data, status.HTTP_201_CREATED)
- 
+
         except Exception as e:
             logger.error("Error creating customer: %s", str(e))
             return build_response(0, "Record creation failed due to an error", [], status.HTTP_500_INTERNAL_SERVER_ERROR)
